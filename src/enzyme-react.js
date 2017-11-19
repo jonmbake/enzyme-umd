412963f1c037b6ae82c7618e3a28b40b5b994256
Jon Bake <jonmbake@gmail.com>
2017-11-19 11:26:39 -0600

    Move dependencies that make up UMD Module to package.dependencies instead of package.devDependencies

--- src/enzyme-react.js
+++ src/enzyme-react.js
@@ -1,6 +1,5 @@
 exports.react = require('react');
 exports['react-dom'] = require('react-dom');
-exports['react-test-renderer'] = require('react-test-renderer');
 var Enzyme = exports.enzyme = require('enzyme');
 var Adapter = require('enzyme-adapter-react-16');
 Enzyme.configure({ adapter: new Adapter() });
